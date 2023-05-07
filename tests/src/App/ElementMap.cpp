e6faaf946cfc82cb381422f2e986248b13889a1e
John Dupuy <jdupuy98@gmail.com>
2023-05-07 15:18:20 -0500

    fixed comments and added first test

--- tests/src/App/ElementMap.cpp
+++ tests/src/App/ElementMap.cpp
@@ -2,17 +2,48 @@
 
 #include "gtest/gtest.h"
 
+#include <App/Application.h>
 #include "App/ElementMap.h"
 
-#include <sstream>
+// NOLINTBEGIN(readability-magic-numbers)
 
 class ElementMapTest: public ::testing::Test
 {
+protected:
+    void SetUp() override {
+        int argc = 1;
+        char *argv[] = { "FreeCAD" };
+        App::Application::Config()["ExeName"] = "FreeCAD";
+        App::Application::init(argc, argv);
+        App::GetApplication().newDocument("test", "testUser");
+    }
+    // void TearDown() override {}
 };
 
 TEST_F(ElementMapTest, defaultConstruction)
 {
     // Act
+    Data::ElementMap elementMap = Data::ElementMap();
 
     // Assert
+    EXPECT_EQ(elementMap.size(), 0);
 }
+
+//TEST_F(ElementMapTest, setElementNameDefaults)
+//{
+//    // Arrange
+//    Data::ElementMapPtr currentElementMap = std::make_shared<Data::ElementMap>();
+//    Data::MappedName mappedName("TEST");
+//    Data::IndexedName element("Edge", 1);
+//    Data::ElementIDRefs _sid;
+//    auto sids = &_sid;
+//    Data::MappedName expectedName("TEST");
+//
+//    // Act
+//    auto newName = elementMap.setElementName(element, mappedName, currentElementMap, 0);
+//
+//    // Assert
+//    EXPECT_EQ(newName, expectedName);
+//}
+
+// NOLINTEND(readability-magic-numbers)
